6623f35ba675b52ff6d68214be7421a8c7d567f6
Charlie Grover <grover47@hotmail.com>
2023-10-22 14:31:08 +0000

    # 29 refactor s3 bucket into new terraform_module

--- providers.tf
+++ providers.tf
@@ -1,28 +1,3 @@
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-}
-
-terraform {
-  cloud {
-    organization = "terraform_beginner_bootcamp"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-}
 
 
-provider "aws" {
-
-}
-
-provider "random" {
- 
-}
