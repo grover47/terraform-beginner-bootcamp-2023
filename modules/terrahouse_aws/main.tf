2a3587fb63a353e715d702bb8b1ed1d05fea6840
Charlie Grover <grover47@hotmail.com>
2023-10-22 19:19:53 +0000

    # 31 enabled ststaic web site hosting and uploaded index and error file via terraform

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -17,3 +17,33 @@ resource "aws_s3_bucket" "website_bucket" {
     UserUuid = var.user_uuid
   }
 }
+
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "index.html"
+  source = var.index_html_filepath
+  etag = filemd5("${path.root}/public/index.html")
+}
+
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "error.html"
+  source = "${path.root}/public/error.html"
+  etag = filemd5("${path.root}/public/error.html")
+}
+
+
